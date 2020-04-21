9f426ad02a0bbd6fa769650cfb0726f9e4033c1d
Martin Reese <github@reeses.eu>
2020-04-21 08:17:03 +0200

    workout attributes

--- lib/workout.py
+++ lib/workout.py
@@ -3,7 +3,7 @@
 import logging
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Date, DateTime
+from sqlalchemy import Column, String, Integer, Numeric, DateTime
 from sqlalchemy import ForeignKey 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -28,7 +28,7 @@ class Sport(Base):
       id = self.id
     return id
 
-  def get(database, name):
+  def get(self, database, name):
     result = database.session.query(Sport).filter(Sport.name == name).all()
     if result:
       return result[0]
@@ -70,7 +70,7 @@ class SportsType(Base):
       id = self.id
     return id
 
-  def get(database, name):
+  def get(self, database, name):
     result = database.session.query(SportsType).filter(SportsType.name == name).all()
     if result:
       return result[0]
@@ -84,13 +84,89 @@ class SportsType(Base):
 class Workout(Base):
   __tablename__ = 'workouts'
   
-  id            = Column(Integer, primary_key = True)
-  source        = Column(String(32))
-  source_ref    = Column(Integer) 
-  sportstype_id = Column(Integer, ForeignKey('sportstypes.id'))
-  sport_id      = Column(Integer, ForeignKey('sports.id'))
-  name          = Column(String) 
-  date          = Column(Date)
+  # identification
+  id                                    = Column(Integer, primary_key = True)
+  source                                = Column(String(32))
+  external_id                           = Column(Integer)
+  # sportstype 
+  sportstype_id                         = Column(Integer, ForeignKey('sportstypes.id'))
+  sport_id                              = Column(Integer, ForeignKey('sports.id'))
+  # description
+  name                                  = Column(String) 
+  description                           = Column(String)
+  min_temperature                       = Column(Numeric, precision=4, scale=1)
+  max_temperature                       = Column(Numeric, precision=4, scale=1)
+  # time
+  start_time                            = Column(DateTime)
+  duration_sec                          = Column(Integer)
+  moving_duration_sec                   = Column(Integer)
+  # key performance indicators
+  distance_m                            = Column(Integer)
+  average_speed_m_per_sec               = Column(Numeric, precision=7, scale=2)
+  max_speed_m_per_sec                   = Column(Numeric, precision=7, scale=2)
+  elevation_gain_m                      = Column(Integer)
+  elevation_loss_m                      = Column(Integer)
+  calories                              = Column(Integer)
+  average_hr                            = Column(Integer)
+  max_hr                                = Column(Integer)
+  avg_power                             = Column(Integer)
+  max_power                             = Column(Integer)
+  norm_power                            = Column(Integer)
+  # training effect
+  aerobic_training_effect               = Column(Numeric, precision=4, scale=1)
+  anaerobic_training_effect             = Column(Numeric, precision=4, scale=1)
+  training_stress_score                 = Column(Integer)
+  intensity_factor                      = Column(Integer)
+  # cadences
+  average_running_cadence_steps_per_min = Column(Integer)
+  max_running_cadence_steps_per_min     = Column(Integer)
+  average_biking_cadence_rev_per_min    = Column(Integer)
+  max_biking_cadence_rev_per_min        = Column(Integer)
+  average_swim_cadence_strokes_per_min  = Column(Integer)
+  max_swim_cadence_strokes_per_min      = Column(Integer)
+  # swimming specific
+  average_swolf                         = Column(Integer)
+  active_lengths                        = Column(Integer)
+  pool_length                           = Column(Integer)
+  unit_of_pool_length                   = Column(Integer)
+  strokes                               = Column(Integer)
+  avg_stroke_distance                   = Column(Integer)
+  avg_stroke_cadence                    = Column(Integer)
+  max_stroke_cadence                    = Column(Integer)
+  avg_strokes                           = Column(Integer)
+  min_strokes                           = Column(Integer)
+  # running specific
+  left_balance                          = Column(Numeric, precision=5, scale=2)
+  right_balance                         = Column(Numeric, precision=5, scale=2)
+  avg_left_balance                      = Column(Numeric, precision=5, scale=2)
+  avg_vertical_oscillation              = Column(Numeric, precision=5, scale=2)
+  avg_ground_contact_time               = Column(Integer)
+  avg_stride_length                     = Column(Integer)
+  avg_fractional_cadence                = Column(Integer)
+  max_fractional_cadence                = Column(Integer)
+  avg_vertical_ratio                    = Column(Integer)
+  avg_ground_contact_balance            = Column(Integer)
+  # fitness level
+  vo2_max_value                         = Column(Integer)
+  lactate_threshold_bpm                 = Column(Integer)
+  lactate_threshold_speed               = Column(Integer)
+  max_ftp                               = Column(Integer)
+  max_20_min_power                      = Column(Integer)
+  max_avg_power_1                       = Column(Integer)
+  max_avg_power_2                       = Column(Integer)
+  max_avg_power_5                       = Column(Integer)
+  max_avg_power_10                      = Column(Integer)
+  max_avg_power_20                      = Column(Integer)
+  max_avg_power_30                      = Column(Integer)
+  max_avg_power_60                      = Column(Integer)
+  max_avg_power_120                     = Column(Integer)
+  max_avg_power_300                     = Column(Integer)
+  max_avg_power_600                     = Column(Integer)
+  max_avg_power_1200                    = Column(Integer)
+  max_avg_power_1800                    = Column(Integer)
+  max_avg_power_3600                    = Column(Integer)
+  max_avg_power_7200                    = Column(Integer)
+  max_avg_power_18000                   = Column(Integer)
 
   def __repr__(self):
     return "({}) {} from {} doing {} ({}) imported by {}".format(self.id, self.name, self.date, self.sportstype_id, self.sport_id, self.source)
@@ -136,82 +212,5 @@ class WorkoutsDatabase:
     for workout in workouts:
       print(workout)
 
+  
 
-
-"""
-  def _to_workout(workout)  
-    activity = Activity.new
-    activity.record[:source] = "garmin"
-    activity.record[:id] = garmin["activityId"]
-    activity.record[:name]  = garmin["activityName"]
-    activity.record[:description]  = garmin["description"]
-    activity.record[:start_time]  = garmin["startTimeLocal"]
-    activity.record[:sport_type]  = garmin["activityType.typeKey"]
-    activity.record[:comments] = garmin["comments"]
-    activity.record[:parent_id] = garmin["parentId"]
-    activity.record[:distance] = garmin["distance"].to_i
-    activity.record[:duration] = garmin["duration"] ? (garmin["duration"].to_i/60) : garmin["duration"]
-    activity.record[:moving_duration] = garmin["movingDuration"] ? (garmin["movingDuration"].to_i/60) : garmin["movingDuration"]
-    activity.record[:elevation_gain] = garmin["elevationGain"].to_i
-    activity.record[:elevation_loss] = garmin["elevationLoss"].to_i
-    activity.record[:average_speed] = garmin["averageSpeed"] ? (garmin["averageSpeed"] * 3.6).to_f.round(2) : garmin["averageSpeed"]
-    activity.record[:max_speed] = garmin["maxSpeed"] ? (garmin["maxSpeed"] * 3.6).to_f.round(2) : garmin["maxSpeed"]
-    activity.record[:calories] = garmin["calories"].to_i
-    activity.record[:average_hr] = garmin["averageHR"].to_i
-    activity.record[:max_hr] = garmin["maxHR"].to_i
-    activity.record[:average_running_cadence_in_steps_per_minute] = garmin["averageRunningCadenceInStepsPerMinute"].to_i
-    activity.record[:max_running_cadence_in_steps_per_minute] = garmin["maxRunningCadenceInStepsPerMinute"].to_i
-    activity.record[:average_biking_cadence_in_rev_per_minute] = garmin["averageBikingCadenceInRevPerMinute"].to_i
-    activity.record[:max_biking_cadence_in_rev_per_minute] = garmin["maxBikingCadenceInRevPerMinute"].to_i
-    activity.record[:average_swim_cadence_in_strokes_per_minute] = garmin["averageSwimCadenceInStrokesPerMinute"].to_i
-    activity.record[:max_swim_cadence_in_strokes_per_minute] = garmin["maxSwimCadenceInStrokesPerMinute"].to_i
-    activity.record[:average_swolf] = garmin["averageSwolf"].to_i
-    activity.record[:active_lengths] = garmin["activeLengths"].to_i
-    activity.record[:pool_length] = garmin["poolLength"].to_i
-    activity.record[:unit_of_pool_length] = garmin["unitOfPoolLength"].to_i
-    activity.record[:avg_power] = garmin["avgPower"].to_i
-    activity.record[:max_power] = garmin["maxPower"].to_i
-    activity.record[:aerobic_training_effect] = garmin["aerobicTrainingEffect"].to_f.round(1)
-    activity.record[:anaerobic_training_effect] = garmin["anaerobicTrainingEffect"].to_f.round(1)
-    activity.record[:strokes] = garmin["strokes"].to_i
-    activity.record[:norm_power] = garmin["normPower"].to_i
-    activity.record[:left_balance] = garmin["leftBalance"]
-    activity.record[:right_balance] = garmin["rightBalance"]
-    activity.record[:avg_left_balance] = garmin["avgLeftBalance"]
-    activity.record[:max_20_min_power] = garmin["max20MinPower"].to_i
-    activity.record[:avg_vertical_oscillation] = garmin["avgVerticalOscillation"].to_f.round(2)
-    activity.record[:avg_ground_contact_time] = garmin["avgGroundContactTime"].to_i
-    activity.record[:avg_stride_length] = garmin["avgStrideLength"].to_i
-    activity.record[:avg_fractional_cadence] = garmin["avgFractionalCadence"].to_i
-    activity.record[:max_fractional_cadence] = garmin["maxFractionalCadence"].to_i
-    activity.record[:training_stress_score] = garmin["trainingStressScore"].to_i
-    activity.record[:intensity_factor] = garmin["intensityFactor"].to_i
-    activity.record[:vo2_max_value] = garmin["vO2MaxValue"].to_i
-    activity.record[:avg_vertical_ratio] = garmin["avgVerticalRatio"].to_i
-    activity.record[:avg_ground_contact_balance] = garmin["avgGroundContactBalance"].to_i
-    activity.record[:lactate_threshold_bpm] = garmin["lactateThresholdBpm"].to_i
-    activity.record[:lactate_threshold_speed] = garmin["lactateThresholdSpeed"].to_i
-    activity.record[:max_ftp] = garmin["maxFtp"].to_i
-    activity.record[:avg_stroke_distance] = garmin["avgStrokeDistance"].to_i
-    activity.record[:avg_stroke_cadence] = garmin["avgStrokeCadence"].to_i
-    activity.record[:max_stroke_cadence] = garmin["maxStrokeCadence"].to_i
-    activity.record[:avg_strokes] = garmin["avgStrokes"].to_i
-    activity.record[:min_strokes] = garmin["minStrokes"].to_i
-    activity.record[:min_temperature] = garmin["minTemperature"].to_f.round(1)
-    activity.record[:max_temperature] = garmin["maxTemperature"].to_f.round(1)
-    activity.record[:max_avg_power_1] = garmin["maxAvgPower_1"]
-    activity.record[:max_avg_power_2] = garmin["maxAvgPower_2"]
-    activity.record[:max_avg_power_5] = garmin["maxAvgPower_5"]
-    activity.record[:max_avg_power_10] = garmin["maxAvgPower_10"]
-    activity.record[:max_avg_power_20] = garmin["maxAvgPower_20"]
-    activity.record[:max_avg_power_30] = garmin["maxAvgPower_30"]
-    activity.record[:max_avg_power_60] = garmin["maxAvgPower_60"]
-    activity.record[:max_avg_power_120] = garmin["maxAvgPower_120"]
-    activity.record[:max_avg_power_300] = garmin["maxAvgPower_300"]
-    activity.record[:max_avg_power_600] = garmin["maxAvgPower_600"]
-    activity.record[:max_avg_power_1200] = garmin["maxAvgPower_1200"]
-    activity.record[:max_avg_power_1800] = garmin["maxAvgPower_1800"]
-    activity.record[:max_avg_power_3600] = garmin["maxAvgPower_3600"]
-    activity.record[:max_avg_power_7200] = garmin["maxAvgPower_7200"]
-    activity.record[:max_avg_power_18000] = garmin["maxAvgPower_18000"]
-"""
